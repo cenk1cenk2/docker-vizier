e494f95fd758d6416a0eda003a135a6ee851a1c1
Cenk Kılıç <cenk@kilic.dev>
2023-11-03 19:44:58 +0100

    ci: add ci build

---
+++ release.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  branches: [
+    "main",
+    "master",
+    "next",
+    "next-major",
+    {
+      name: "alpha",
+      prerelease: true,
+    },
+    {
+      name: "beta",
+      prerelease: true,
+    },
+    {
+      name: "rc",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@google/semantic-release-replace-plugin",
+      {
+        replacements: [
+          {
+            files: ["version.go"],
+            from: 'const VERSION = "(.*)"',
+            to: 'const VERSION = "v${nextRelease.version}"',
+            results: [
+              {
+                file: "version/v.go",
+                hasChanged: true,
+                numMatches: 1,
+                numReplacements: 1,
+              },
+            ],
+            countMatches: true,
+          },
+        ],
+      },
+    ],
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "README.md", "docs/"],
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd: "echo '${nextRelease.version}' > .tags",
+      },
+    ],
+  ],
+};
--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package main
 
 const CLI_NAME = "vizier"
 
-const DESCRIPTION = "Some description"
+const DESCRIPTION = "Supervisor for running multiple tasks in a Docker container."
 
 const VERSION = "latest"
